eb9111e8168d1ea51ed6d4c2b8810ae597a5ff34
Rohit Hegade <60090023+Rohithegade02@users.noreply.github.com>
2024-06-14 05:30:26 +0530

    removed $ in process.env

--- server/config/db.js
+++ server/config/db.js
@@ -4,10 +4,10 @@ dotenv.config()
 
 //mysql db connection
 const db = mysql.createPool({
-  host: `${process.env.HOST}`,
-  user: `${process.env.USER}`,
-  password: `${process.env.Password}`,
-  database: `${process.env.DATABASE}`,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.Password,
+  database: process.env.DATABASE,
   insecureAuth: true
 });
 
